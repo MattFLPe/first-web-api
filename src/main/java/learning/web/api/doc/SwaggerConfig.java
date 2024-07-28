1a073c01a8cf5dc09c71a9627b664589dc687c4d
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-28 12:09:25 -0300

    Add imports, contact and apiInfo methods and document

--- src/main/java/learning/web/api/doc/SwaggerConfig.java
+++ src/main/java/learning/web/api/doc/SwaggerConfig.java
@@ -1,5 +1,57 @@
 package learning.web.api.doc;
 
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
+    private Contact contact() {
+        return new Contact(
+                "Seu nome",
+                "http://www.seusite.com.br",
+                "voce@seusite.com.br");
+    }
+
+    private ApiInfoBuilder apiInformations() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa");
+        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
+        apiInfoBuilder.contact(this.contact());
+
+        return apiInfoBuilder;
+    }
+
+    @Bean
+    public Docket apiDetails() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pacote.comseus.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInformations().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+
+        return docket;
+    }
 }
