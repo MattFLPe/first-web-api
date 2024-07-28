31f8ba6763a367d4c49014b615f696d753e7eb54
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-28 11:24:54 -0300

    Add methods findAllUsers, findOne and deleteUserById

--- src/main/java/learning/web/api/controller/UserController.java
+++ src/main/java/learning/web/api/controller/UserController.java
@@ -3,7 +3,9 @@ package learning.web.api.controller;
 import learning.web.api.model.User;
 import learning.web.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,7 +15,17 @@ public class UserController {
     @Autowired
     private UserRepository repository;
     @GetMapping("/users")
-    public List<User> getUsers() {
+    public List<User> findAllUsers() {
         return repository.findAll();
     }
+    @GetMapping("/users/{username}")
+    public User findOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+    }
 }
+
+
