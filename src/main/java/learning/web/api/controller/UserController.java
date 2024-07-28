41e0359350c715a1cd21d70434a662d56b50f560
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-28 12:07:37 -0300

    Add POST method postUser and @RequestMapping for the /users/ prefix

--- src/main/java/learning/web/api/controller/UserController.java
+++ src/main/java/learning/web/api/controller/UserController.java
@@ -3,28 +3,30 @@ package learning.web.api.controller;
 import learning.web.api.model.User;
 import learning.web.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository repository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<User> findAllUsers() {
         return repository.findAll();
     }
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public User findOne(@PathVariable("username") String username) {
         return repository.findByUsername(username);
     }
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void deleteUserById(@PathVariable("id") Integer id) {
         repository.deleteById(id);
+    }
+    @PostMapping()
+    public void postUser(@RequestBody User user) {
+        repository.save(user);
     }
 }
 
