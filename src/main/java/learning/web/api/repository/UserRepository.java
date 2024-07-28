8f79d04f9248bb69705e6e3cebb4ebbe6a0dbbb7
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-28 19:38:55 -0300

    Add a new if condition for error handling on save method

--- src/main/java/learning/web/api/repository/UserRepository.java
+++ src/main/java/learning/web/api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package learning.web.api.repository;
 
+import learning.web.api.handler.BusinessException;
+import learning.web.api.handler.RequiredFieldException;
 import learning.web.api.model.User;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,10 @@ import java.util.List;
 
 @Repository
 public class UserRepository {
-    public void save(User user){
+    public void save(User user) {
+       if(User.getLogin()==null)
+           throw new RequiredFieldException("login");
+
         if(User.getId()==null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
@@ -23,16 +28,16 @@ public class UserRepository {
     public List<User> findAll(){
         System.out.println("LIST - Listando os usários do sistema");
         List<User> usuarios = new ArrayList<>();
-        usuarios.add(new User("gleyson","password"));
-        usuarios.add(new User("frank","masterpass"));
+        usuarios.add(new User("Matt","password"));
+        usuarios.add(new User("John","masterpass"));
         return usuarios;
     }
     public User findById(Integer id){
         System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
-        return new User("gleyson","password");
+        return new User("Matt","password");
     }
     public User findByUsername(String username){
         System.out.println(String.format("FIND/username - Recebendo o usernamae: %s para localizar um usuário", username));
-        return new User("gleyson","password");
+        return new User("Matt","password");
     }
 }
